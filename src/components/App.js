bc6c178a6bb3e705c59421b628c4e2a3f1809c93
waleed7333 <waleed733150@gmail.com>
2024-09-08 09:14:14 +0300

    update js files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+import NewPlantForm from "./NewPlantForm";
+import Search from "./Search";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const addPlant = (newPlant) => {
+    setPlants((prevPlants) => [...prevPlants, newPlant]);
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <Search setSearchTerm={setSearchTerm} />
+      <NewPlantForm addPlant={addPlant} />
+      <PlantPage plants={filteredPlants} />
     </div>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,15 +1,12 @@
 import React from "react";
-import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
-import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants }) {
   return (
-    <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
-    </main>
+    <div className="plant-page">
+      <h1>Plant Collection</h1>
+      <PlantList plants={plants} />
+    </div>
   );
 }
 
